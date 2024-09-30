23ed8fa95705cf26412fe5999d406b7224902045
shanhaider23 <shan.haider@gmail.com>
2024-09-30 18:51:06 +0200

    finalise project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Header, Footer } from './components'; // Works correctly with named exports
 
 function App() {
-	return (
-		<>
-			<h1>App write</h1>
-		</>
-	);
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		authService
+			.getCurrentUser()
+			.then((userData) => {
+				if (userData) {
+					dispatch(login({ userData }));
+				} else {
+					dispatch(logout());
+				}
+			})
+			.finally(() => setLoading(false));
+	}, [dispatch]); // Added dependency array
+
+	return !loading ? (
+		<div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+			<div className="w-full block">
+				<Header />
+				<main>
+					<h1>outlet</h1>
+				</main>
+				{/* <Footer /> */}
+			</div>
+		</div>
+	) : null;
 }
 
 export default App;
